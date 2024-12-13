9705b14d58ade8477c8ac7b240f9b5209849f248
mohamed mourali <mohamedtaha.morali@gmail.com>
2024-12-13 18:05:33 +0100

    modeling

---
+++ backend/src/.ipynb_checkpoints/treatement-checkpoint.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from sklearn.metrics      import confusion_matrix, classification_report
+from sklearn.metrics      import precision_recall_fscore_support as score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+import mlflow
+import warnings
+
+def naive_bayes_model(data_url, version, df, x_train, y_train, x_test, y_test):
+    mlflow.sklearn.autolog(disable=True)
+    with mlflow.start_run(run_name='Naive Bayes'):
+        mlflow.log_param("data_url", data_url)
+        mlflow.log_param("data_version", version)
+        mlflow.log_param("input_rows", df.shape[0])
+        mlflow.log_param("input_cols", df.shape[1])
+        nb = GaussianNB()
+        params = nb.get_params()
+        mlflow.set_tag(key= "model", value="NaiveBayes")
+        mlflow.log_params(params)
+        nb.fit(x_train,y_train)
+        train_features_name = f'{x_train=}'.split('=')[0]
+        train_label_name = f'{y_train=}'.split('=')[0]
+        mlflow.set_tag(key="train_features_name",value= train_features_name)
+        mlflow.set_tag(key= "train_label_name",value=train_label_name)
+        predicted=nb.predict(x_test)
+        precision,recall,fscore,support=score(y_test,predicted,average='macro')
+        mlflow.log_metric("Precision_test",precision)
+        mlflow.log_metric("Recall_test",recall)
+        mlflow.log_metric("F1_score_test",fscore)
+        mlflow.sklearn.log_model(nb,artifact_path="ML_models")
+
+def knn_model(data_url, version, df, x_train, y_train, x_test, y_test):
+    mlflow.sklearn.autolog(disable=True)
+    with mlflow.start_run(run_name='Naive Bayes'):
+        mlflow.log_param("data_url", data_url)
+        mlflow.log_param("data_version", version)
+        mlflow.log_param("input_rows", df.shape[0])
+        mlflow.log_param("input_cols", df.shape[1])
+        knn = KNeighborsClassifier(n_neighbors=3, weights='distance', metric='manhattan')
+        params = knn.get_params()
+        mlflow.set_tag(key="model", value="K-Nearest Neighbors")
+        mlflow.log_params(params)
+        knn.fit(x_train,y_train)
+        train_features_name = f'{x_train=}'.split('=')[0]
+        train_label_name = f'{y_train=}'.split('=')[0]
+        mlflow.set_tag(key="train_features_name",value= train_features_name)
+        mlflow.set_tag(key= "train_label_name",value=train_label_name)
+        predicted=knn.predict(x_test)
+        precision,recall,fscore,support=score(y_test,predicted,average='macro')
+        mlflow.log_metric("Precision_test",precision)
+        mlflow.log_metric("Recall_test",recall)
+        mlflow.log_metric("F1_score_test",fscore)
+        mlflow.sklearn.log_model(nb,artifact_path="ML_models")
+    
---
+++ backend/src/modeling.py
@@ -0,0 +1,45 @@
+#from dotenv import load_dotenv
+import os
+from data_preprocessing_training import transform_data
+import pandas as pd
+from imblearn.over_sampling import SMOTE
+import mlflow
+from treatement import naive_bayes_model
+from treatement import knn_model
+
+#Load environement variable (Dagshub credentials)
+# from dotenv import load_dotenv
+# import os
+# load_dotenv("../backend/src/.env")
+
+# DagsHub_username = os.getenv("DagsHub_username")
+# DagsHub_token=os.getenv("DagsHub_token")
+
+#Get Dagshub credentials
+# os.environ['MLFLOW_TRACKING_USERNAME']= DagsHub_username
+# os.environ["MLFLOW_TRACKING_PASSWORD"] = DagsHub_token
+
+#Affect Daghsub credentials 
+
+os.environ['MLFLOW_TRACKING_USERNAME']= "KoubaaMahdi"
+os.environ["MLFLOW_TRACKING_PASSWORD"] = "666511bfd2b2180d037f16fac402803ea2a8aed1"
+
+#setup mlflow
+mlflow.set_tracking_uri('https://dagshub.com/KoubaaMahdi/MLOps_project.mlflow') #your mlfow tracking uri
+
+#setup mlflow
+mlflow.set_experiment("depression-detection-experiment")
+
+#Data Url and version
+version = "v2.0"
+data_url = "../../data/depression_data.csv"
+
+#read the data
+df = pd.read_csv(data_url)
+
+#cleaning and preprocessing
+X_train,X_test,y_train,y_test = transform_data(df)
+
+#Execute the models with new version of data:
+naive_bayes_model(data_url,version,df,X_resampled,y_resampled,X_test,y_test)
+knn_model(data_url,version,df,X_resampled,y_resampled,X_test,y_test)
